3af674886f9bac53a683b59b4969a8bdbf31649e
Sean Kramer <seandominickramer@gmail.com>
2021-02-10 20:23:43 -0500

    Add endpoint to get all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,28 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const db = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+//get home message
+server.get('/', (req, res) => {
+  res.json({message: 'Welcome to users api'})
+})
+
+//get all users
+server.get('/users', async(req, res) => {
+  const users = await db.find()
+   if(users) {
+     res.json(users)
+   } else {res.status(500).json({
+    message: "The users information could not be retrieved"
+   })}
+})
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
